38facbbe969a707da5985643834e53a0b8db637c
John Morris <john@zultron.com>
2021-02-17 12:46:50 -0600

    Add integration tests for concurrency

--- tests/integration/test_config_manager_integration.py
+++ tests/integration/test_config_manager_integration.py
@@ -1,6 +1,8 @@
 import os
 
 import pytest
+import threading
+import queue
 import rospy
 
 from redis_store import RedisDict, ConfigClient
@@ -282,3 +284,188 @@ def test_reset_params_removes_additional_params_from_redis(node, client, redis):
     assert success is True
     assert rospy.get_param('octonare', None) is None
     assert redis.get('octonare', None) is None
+
+
+class TestConfigClientConcurrencyGetParamSharedClient:
+    # Test get_param() concurrency with threads sharing a ConfigClient
+    # object
+
+    num_iterations = 500
+    name = "get_param_shared"
+
+    param_data = dict(
+        foo=1,
+        bar=2,
+        baz="bleb",
+        quux=dict(x=1, y="flux"),
+    )
+
+    @pytest.fixture
+    def seed_param_data(self, client):
+        # Seed params for test
+        for key, val in self.param_data.items():
+            assert client.set_param(key, val) is True
+
+        return self.param_data
+
+    def thread_client(self):
+        # Share one client among all threads
+        if not hasattr(self, '_thread_client'):
+            self._thread_client = ConfigClient(subscribe=True)
+        return self._thread_client
+
+    def test_multithreaded_client_config(self, node, client, seed_param_data):
+        # Run parallel queries from threads, one client shared across threads
+        # - Threading apparatus
+        self.results = queue.Queue(maxsize=len(self.param_data))
+        threads = dict()
+
+        # - Init and start threads
+        print("\n{self.name}:  Setup\n")
+        for key, val in self.param_data.items():
+            threads[key] = threading.Thread(
+                target=self.thread_func,
+                args=(key,),
+            )
+        print("{self.name}:  Start\n")
+        for key in self.param_data.keys():
+            threads[key].start()
+
+        # - Wait for thread finish
+        print("{self.name}:  Wait\n")
+        for key, val in self.param_data.items():
+            threads[key].join()
+
+        # - Read and verify results
+        print(f"{self.name}:  Finished with {self.results.qsize()} results\n")
+        assert self.results.qsize() == len(self.param_data)
+        while not self.results.empty():
+            self.verify_result(*self.results.get())
+
+    def thread_func(self, key):
+        # Read and verify parameter in a loop, then record results
+        failures = 0
+        exceptions = 0
+        count = 0
+        val = self.param_data[key]
+        client = self.thread_client()
+        print(f"\n{self.name}:  thread_func:  {key} = {val}")
+        print(f"{self.name}:      client:  {client}")
+        for test_iter in range(self.num_iterations):
+            count += 1
+            try:
+                param_val = client.get_param(key)
+            except Exception as e:
+                print(
+                    f"{self.name}:  thread_func {key} exception at {count}:"
+                    f"  {str(e)}"
+                )
+                exceptions += 1
+                continue
+            if param_val != val:
+                failures += 1
+                print(
+                    f"{self.name}:  thread_func {key} failure at {count}:  "
+                    f"{param_val} != {val}"
+                )
+            else:
+                print(f"{self.name}:  thread_func:  {key} success at {count}")
+        self.results.put((key, val, count, failures, exceptions))
+        print(
+            f"{self.name}:  thread_func:  {key} {failures}+{exceptions}/{count}"
+        )
+
+    def verify_result(self, key, val, count, failures, exceptions):
+        print(
+            f"{self.name}:  Result {key}='{val}' "
+            f"failures+exceptions/count:  {failures}+{exceptions}/{count}"
+        )
+        assert self.param_data[key] == val  # Sanity check
+        assert count == self.num_iterations
+        assert failures == 0
+        assert exceptions == 0
+
+
+class TestConfigClientConcurrencyGetParamUnsharedClient(
+    TestConfigClientConcurrencyGetParamSharedClient
+):
+    # Test get_param() concurrency with threads using separate
+    # ConfigClient objects
+
+    name = "get_param_unshared"
+
+    def thread_client(self):
+        # Use new client for each thread
+        return ConfigClient(subscribe=True)
+
+
+class TestConfigClientConcurrencySetParam(
+    TestConfigClientConcurrencyGetParamSharedClient
+):
+    # Test set_param() concurrency with threads sharing a ConfigClient
+    # object
+    #
+    # The threads iterate, each reading a parameter, incrementing it
+    # and writing it back; starting values are set to prevent overlap
+    # so problems are more evident
+
+    num_iterations = 500
+    name = "set_param"
+
+    param_data = dict(
+        foo=1000,
+        bar=2000,
+        baz=3000,
+        quux=4000,
+    )
+
+    def thread_func(self, key):
+        # Read, increment and write parameter in a loop, then record
+        # results
+        failures = 0
+        exceptions = 0
+        start_val = self.param_data[key]
+        client = self.thread_client()
+        val = client.get_param(key)
+        print(f"\n{self.name}:  {key} = {start_val} client:  {client}")
+        if val != start_val:
+            print(
+                f"{self.name}:  {key} initial val failed {val} != {start_val}"
+            )
+            failures += 1
+        for test_iter in range(self.num_iterations):
+            try:
+                last_val = client.get_param(key)
+            except Exception as e:
+                print(f"{self.name}:  {key} exception at {val}:  {str(e)}")
+                exceptions += 1
+            else:
+                if last_val != val:
+                    failures += 1
+                    print(f"{self.name}:  {key} failure {last_val} != {val}")
+            val += 1
+
+            try:
+                if client.set_param(key, val):
+                    print(f"{self.name}:  set {key} = {val}")
+                else:
+                    print(f"{self.name}:  failure to set {key} = {val}")
+                    failures += 1
+            except Exception as e:
+                print(
+                    f"{self.name}:  exception setting {key} = {val}:  {str(e)}"
+                )
+                exceptions += 1
+
+        self.results.put((key, val, failures, exceptions))
+        print(f"{self.name}:  {key} {failures}+{exceptions}/{val}")
+
+    def verify_result(self, key, val, failures, exceptions):
+        start_val = self.param_data[key]
+        iters = val - start_val
+        print(
+            f"{self.name}:  Fail/except {key}:  {failures}+{exceptions}/{iters}"
+        )
+        assert iters == self.num_iterations
+        assert failures == 0
+        assert exceptions == 0
